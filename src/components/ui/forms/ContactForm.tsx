3cf1d581d636bad3e4845ca50a71f83ddd1f48a9
Noah Milberger <n.milberger@gmail.com>
2023-06-01 09:04:03 -0400

    Added emailJS for contact us form functionality.

--- src/components/ui/forms/ContactForm.tsx
+++ src/components/ui/forms/ContactForm.tsx
@@ -1,22 +1,51 @@
 "use client";
 
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, UseFormSetValue, useForm } from "react-hook-form";
 import { FormProps } from ".";
 import { Box, Grid } from "@mui/material";
 import TextField from "../form-fields/TextField";
 import { REGEX_EMAIL } from "@/components/utils/regex";
 import Button from "../buttons/Button";
+import emailjs from "@emailjs/browser";
 
-export default function ContactForm({ loading, onSubmit }: FormProps) {
+// Credentials for EmailJS
+const emailJSAccessToken = process.env.NEXT_PUBLIC_EMAILJS_ACCESS_TOKEN;
+const emailJSServiceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+const emailJSTempleId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+
+const onSubmit = ({
+  data,
+  setValue,
+}: {
+  data: any;
+  setValue: UseFormSetValue<FieldValues>;
+}) => {
+  Object.keys(data).map((keys) => setValue(`${keys}`, ""));
+  const templateParams = { ...data };
+  if (emailJSAccessToken && emailJSServiceId && emailJSTempleId) {
+    emailjs
+      .send(
+        emailJSServiceId,
+        emailJSTempleId,
+        templateParams,
+        emailJSAccessToken
+      )
+      .then(() => console.log("Success!: "))
+      .catch((error) => console.log("Error: ", error));
+  }
+};
+
+export default function ContactForm({ loading }: FormProps) {
   // React Hook Form variables
   const {
     control,
     formState: { errors },
+    setValue,
     handleSubmit,
   } = useForm<FieldValues>();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit((data) => onSubmit({ data, setValue }))}>
       <Box sx={{ flexGrow: 1 }} className="w-full max-w-2xl">
         <Grid container columnSpacing={3}>
           <Grid item xs={12}>
